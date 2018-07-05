63489b6ce8df214ba820100e595391a0fd5d66af
Jim Reed <jim.reed@qlik.com>
2018-07-05 11:35:07 -0400

    serve the mashup using caddy
    serve the mashup using caddy

---
+++ src/extensions/cereal/cereal.js
@@ -0,0 +1,36 @@
+/*
+ * Basic responsive mashup template
+ * @owner Enter you name here (xxx)
+ */
+/*
+ *    Fill in host and port for Qlik engine
+ */
+var prefix = window.location.pathname.substr( 0, window.location.pathname.toLowerCase().lastIndexOf( "/extensions" ) + 1 );
+var config = {
+	host: window.location.hostname,
+	prefix: prefix,
+	port: window.location.port,
+	isSecure: window.location.protocol === "https:"
+};
+require.config( {
+	baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources"
+} );
+
+require( ["js/qlik"], function ( qlik ) {
+	qlik.setOnError( function ( error ) {
+		$( '#popupText' ).append( error.message + "<br>" );
+		$( '#popup' ).fadeIn( 1000 );
+	} );
+	$( "#closePopup" ).click( function () {
+		$( '#popup' ).hide();
+	} );
+
+	//callbacks -- inserted here --
+	//open apps -- inserted here --
+	var app = qlik.openApp('6a0d2cfb-c91b-401f-9e63-e24769dba7d9', config);
+
+	//get objects -- inserted here --
+	app.getObject('QV01','rfkpzp');
+	//create cubes and lists -- inserted here --
+
+} );
